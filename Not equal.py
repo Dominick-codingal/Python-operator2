d8b3b90d195ccbfe8829c4b9dba95e5b2b86a410
Dominic-codingal <dominickcodingal@gmail.com>
2025-04-17 12:31:37 +0300

    first commit

---
+++ Not equal.py
@@ -0,0 +1,21 @@
+a=10
+b=12
+c=12
+print(a!=b)
+print(b!=c)
+
+a = "python"
+b="coding"
+
+if a!=b:
+    print(a, 'and', b, "are different.")
+
+
+a=56
+b= 78   
+if(a==1)!=(b==78):
+    print('different')
+
+a=int(input('Enter a Number: '))    
+if a%2 !=0:
+     print(a, 'Is not even Number.')
